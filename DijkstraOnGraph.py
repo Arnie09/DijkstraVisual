09ad9b06c76e19b3837fe1e9bd95ea5b09b39268
Arnab Chanda <arnabchanda964@gmail.com>
2019-11-19 19:50:45 +0530

    made the class structure

--- DijkstraOnGraph.py
+++ DijkstraOnGraph.py
@@ -1,95 +1,83 @@
 from collections import deque
 
-'''Input is a where the graph'''
+class DijkstraGraph:
 
-graph =[[0,0, 0, 0,'O',0,0],
-        [0,5,'O',0,'O',0,0],
-        [0,0,'O',0,'O',0,0],
-        [0,0,'O',0,0,10,0],
-        [0,0, 0, 0,'O',0,0]]
+    def __init__(self,starting,ending,obstacles):
+        self.row = 5
+        self.column = 7
+        self.startingX = starting[0]
+        self.startingY = starting[1]
+        self.finalX = ending[0]
+        self.finalY = ending[1]
+        self.obstacles = obstacles
 
-n = 7
-m = 5
+        self.adjacency_matrix = {}
+        self.distance_mat = []
+        self.visited = set([])
+        self.shorted = deque([])
 
-adjacency_matrix = {}
+        for i in range(self.row):
+            for j in range(self.column):
 
-for i in range(m):
-    for j in range(n):
-        #print(i,j)
-        adjacency_matrix[(i,j)] = []
-        if i+1<m:
-            adjacency_matrix[(i,j)].append([i+1,j])
-        if i-1>=0:
-            adjacency_matrix[(i,j)].append([i-1,j])
-        if j-1>=0:
-            adjacency_matrix[(i,j)].append([i,j-1])
-        if j+1<n:
-            adjacency_matrix[(i,j)].append([i,j+1])
+                self.adjacency_matrix[(i,j)] = []
+                if i+1<self.row:
+                    self.adjacency_matrix[(i,j)].append([i+1,j])
+                if i-1>=0:
+                    self.adjacency_matrix[(i,j)].append([i-1,j])
+                if j-1>=0:
+                    self.adjacency_matrix[(i,j)].append([i,j-1])
+                if j+1<self.column:
+                    self.adjacency_matrix[(i,j)].append([i,j+1])
 
-startingX = 1
-startingY = 1 
-finalX = 3
-finalY = 5
-distance_mat = []
-visited = set([])
-shorted = deque([])
+        self.distance_mat = [[9999999]*self.column for i in range(self.row)]
 
-def calculateShortestPath(xCoordinate,yCoordinate):
-    if xCoordinate == startingX and yCoordinate == startingY:
-        print(xCoordinate,yCoordinate)
-        return
-    else:
-        print(xCoordinate,yCoordinate,"-----",end = " ")
-        adjacent_points = adjacency_matrix[(xCoordinate,yCoordinate)]
-        listOfPoinnts = []
-        for i in adjacent_points:
-            dist = distance_mat[i[0]][i[1]]
-            if dist!=-1:
-                listOfPoinnts.append([dist,[i[0],i[1]]])
-        listOfPoinnts = sorted(listOfPoinnts)
-        #print(listOfPoinnts[0][1],"-----",end = " ")
-        calculateShortestPath(listOfPoinnts[0][1][0],listOfPoinnts[0][1][1])
+        self.distance_mat[self.startingX][self.startingY] = 0
 
-def dijkstra(x,y):
-    shorted.append([x,y])
-    while(len(visited)<m*n):
-        
-        x,y = shorted.popleft()
-        if x == finalX and y == finalY:
-            calculateShortestPath(x,y)
+        for i in range(self.row):
+            for j in range(self.column):
+                if [i,j] in self.obstacles:
+                    self.distance_mat[i][j] = -1
+
+        self.dijkstra(self.startingX,self.startingY)
+        for i in range(self.row):
+            print(*self.distance_mat[i])
+
+
+    def calculateShortestPath(self,xCoordinate,yCoordinate):
+        if xCoordinate == self.startingX and yCoordinate == self.startingY:
+            print(xCoordinate,yCoordinate)
             return
-        if (x,y) in visited:
-            continue
-        visited.add((x,y))
-        adjacentpoints = adjacency_matrix[(x,y)]
-        for i in adjacentpoints:
+        else:
+            print(xCoordinate,yCoordinate,"-----",end = " ")
+            adjacent_points = self.adjacency_matrix[(xCoordinate,yCoordinate)]
+            listOfPoinnts = []
+            for i in adjacent_points:
+                dist = self.distance_mat[i[0]][i[1]]
+                if dist!=-1:
+                    listOfPoinnts.append([dist,[i[0],i[1]]])
+            listOfPoinnts = sorted(listOfPoinnts)
+            self.calculateShortestPath(listOfPoinnts[0][1][0],listOfPoinnts[0][1][1])
 
-            if distance_mat[i[0]][i[1]] == -1:
-                visited.add((i[0],i[1]))
-            elif (i[0],i[1]) not in visited:
-                shorted.append([i[0],i[1]])
-                if distance_mat[i[0]][i[1]]>distance_mat[x][y]+1:
-                    distance_mat[i[0]][i[1]] = distance_mat[x][y]+1
+    def dijkstra(self,x,y):
+        self.shorted.append([x,y])
+        while(len(self.visited)<self.row*self.column):
+            
+            x,y = self.shorted.popleft()
+            if x == self.finalX and y == self.finalY:
+                self.calculateShortestPath(x,y)
+                return
+            if (x,y) in self.visited:
+                continue
+            self.visited.add((x,y))
+            adjacentpoints = self.adjacency_matrix[(x,y)]
+            
+            for i in adjacentpoints:
                 
-    
-    
+                if self.distance_mat[i[0]][i[1]] == -1:
+                    self.visited.add((i[0],i[1]))
+                elif (i[0],i[1]) not in self.visited:
+                    self.shorted.append([i[0],i[1]])
+                    if self.distance_mat[i[0]][i[1]]>self.distance_mat[x][y]+1:
+                        self.distance_mat[i[0]][i[1]] = self.distance_mat[x][y]+1
 
-
-
-for i in range(m):
-    col = []
-    for j in range(n):
-        col.append(999999)
-    distance_mat.append(col)
-
-distance_mat[startingX][startingY] = 0
-
-for i in range(m):
-    for j in range(n):
-        if graph[i][j] == 'O':
-            distance_mat[i][j] = -1
-
-dijkstra(startingX,startingY)
-print()
-for i in range(m):
-    print(*distance_mat[i])
+obj = DijkstraGraph([1,1],[3,5],[[0,3],[1,3],[2,3],[3,3]])
