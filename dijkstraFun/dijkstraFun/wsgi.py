e7836485688a56d95fedbed55c731f1c01d7de33
Arnab Chanda <arnabchanda964@gmail.com>
2019-11-20 22:47:22 +0530

    trying to fix error 3

--- dijkstraFun/dijkstraFun/wsgi.py
+++ dijkstraFun/dijkstraFun/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dijkstraFun.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dijkstraFun.dijkstraFun.settings')
 
 application = get_wsgi_application()
