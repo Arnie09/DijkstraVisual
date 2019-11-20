9ff63175f45eb5fdb02bc2af56b6c2004b080579
Arnab Chanda <arnabchanda964@gmail.com>
2019-11-20 23:05:33 +0530

    trying to fix error 7

--- dijkstraFun/dijkstraFun/settings.py
+++ dijkstraFun/dijkstraFun/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'dijkstraFun.dijkstraFun.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'dijkstraFun', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
